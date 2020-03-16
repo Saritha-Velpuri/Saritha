a5bd68c025d48b2dca20009e88a4e55ba2e6d4d3
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-16 16:23:25 +0530

    Add files via upload

---
+++ Shorthand.java
@@ -0,0 +1,16 @@
+class Shorthand
+ {
+      public static void main(String[] args)         
+      {
+   	 int x;
+
+         x = 3;
+   	 x += 6;
+   	 System.out.println(x);
+    
+   	 x = 3;
+   	 x *= 6;
+   	 System.out.println(x);
+   
+      }
+  }
