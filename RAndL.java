c51b3a66a1ab342bf11ac763478662d098630620
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-16 17:32:49 +0530

    Add files via upload

---
+++ RAndL.java
@@ -0,0 +1,10 @@
+class RAndL 
+{ 
+    public static void main(String args[])  
+   { 
+       int x = 4; 
+       System.out.println(x>>1);
+       System.out.println(x<<1);
+        
+    }     
+} 
