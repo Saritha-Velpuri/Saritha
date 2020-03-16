2f375a29f7be9ecdd247596ad60eb631dd8421cd
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-16 17:01:59 +0530

    Add files via upload

---
+++ PreAndPostIncrement.java
@@ -0,0 +1,14 @@
+class PreAndPostIncrement
+{
+    public static void main(String arg[])
+    {
+        int a = 5;
+        int b = 2;
+        int i = ++a;
+        int j = b++;
+        
+        System.out.println(i);
+        System.out.println(j);
+    
+    }
+}
