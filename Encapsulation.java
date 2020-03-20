e0aa63f7272cfbe44882ed7eb8530ea627f2c654
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-20 19:45:05 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,31 @@
+class Encapsulation
+{
+public static void main(String[] args)
+{
+Emp e1 = new Emp();
+e1.setEmpId(3);
+e1.setEmpName("Encapsulation");
+System.out.println(e1.getEmpName());
+}
+}
+class Emp
+{
+    private int empId;
+    private String empName;
+    public int getEmpId()
+    {
+        return empId;
+    }
+    public void setEmpId(int empId)
+    {
+        this.empId = empId;
+    }
+    public String getEmpName()
+    {
+        return empName;
+    }
+    public void setEmpName(String empName)
+    {
+        this.empName = empName;
+    }
+}
