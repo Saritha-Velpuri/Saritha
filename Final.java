d22fdb2393dbfbf6108f33d6f2b1b205b5c96420
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-21 20:07:26 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,15 @@
+class Final
+{
+    public static void main(String[] args)
+    {
+     A obj=new A();
+     obj.show();
+    }
+}
+final class A
+{
+final public void show()
+{
+  System.out.println("final show");
+}
+}
