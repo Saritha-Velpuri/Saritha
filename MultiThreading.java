437d1c28cf27e233df899330c1e911ff697cf655
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-03-22 09:45:42 +0530

    Add files via upload

---
+++ MultiThreading.java
@@ -0,0 +1,36 @@
+class Hi2 implements Runnable
+{
+	public void run() 
+	{
+		for(int i=1;i<=5;i++) 
+		{
+			System.out.println("Hi");               
+			try {Thread.sleep(1000); }catch(Exception e){}
+		}
+	}
+}
+class Hello2 implements Runnable
+{           
+	public void run() 
+	{
+		for(int i=1;i<=5;i++) 
+		{
+			System.out.println("Hello");
+			try {Thread.sleep(1000); }catch(Exception e){}
+		}
+	}
+}
+public class MultiThreading 
+{
+	public static void main(String[] args) 
+	{
+		Runnable obj1=new Hi2();
+		Runnable obj2=new Hello2();
+		Thread t1=new Thread(obj1);
+		Thread t2=new Thread(obj2);
+		t1.start();
+		t2.start();
+	}
+}                                    
+
+
