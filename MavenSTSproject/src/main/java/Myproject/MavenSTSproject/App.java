524fe02024f66765bc0551d05c77dcea01499da7
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-10-23 12:54:38 +0530

    Add files via upload

---
+++ MavenSTSproject/src/main/java/Myproject/MavenSTSproject/App.java
@@ -0,0 +1,13 @@
+package Myproject.MavenSTSproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
